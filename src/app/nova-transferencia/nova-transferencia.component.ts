b54f50b839e1c61c05c3b82d9723a9450f781889
JhonatanLira <jhonatanlira@gmail.com>
2021-10-05 08:46:59 -0300

    Evento de transferencia @output

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
-  templateUrl:'./nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.component.scss']
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenicaComponent{
+export class NovaTransferenicaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+
+    console.log('Solicitar nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
